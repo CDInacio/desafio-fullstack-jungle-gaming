5521f21ce332b7dab2075f117f52da8953572d45
claudiodantas <claudiodantas1996@gmail.com>
2025-10-31 13:48:41 -0300

    refactor: streamline task creation process and improve error handling

--- apps/task-service/src/app.service.ts
+++ apps/task-service/src/app.service.ts
@@ -2,7 +2,7 @@ import { RpcException } from '@nestjs/microservices';
 import { InjectRepository } from '@nestjs/typeorm';
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateTaskDto, TaskPriority, TaskStatus } from '@repo/shared/task';
-import { Repository } from 'typeorm';
+import { Repository, type DataSource } from 'typeorm';
 import { TaskEntity } from '@repo/shared/entities/task';
 
 @Injectable()
@@ -10,27 +10,28 @@ export class AppService {
   constructor(
     @InjectRepository(TaskEntity)
     private taskRepository: Repository<TaskEntity>,
+    private dataSource: DataSource,
   ) {}
 
   async createTask(task: CreateTaskDto) {
     try {
-      const newTask = new TaskEntity();
-      newTask.title = task.title;
-      newTask.description = task.description;
-      newTask.status = TaskStatus[task.status ?? TaskStatus.TODO];
-      newTask.priority = TaskPriority[task.priority ?? TaskPriority.MEDIUM];
-      newTask.deadline = task.deadline;
-      newTask.createdBy = task.createdBy;
-
-      const respose = await this.taskRepository.save(newTask);
-      return respose;
-    } catch (error) {
-      throw new RpcException({
-        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
-        message: 'Failed to create task',
-        error: error.message,
+      const result = await this.dataSource.transaction(async (manager) => {
+        const newTask = manager.create(TaskEntity, {
+          title: task.title,
+          description: task.description,
+          status: TaskStatus.IN_PROGRESS,
+          priority: TaskPriority.MEDIUM,
+        });
+        return await manager.save(newTask);
       });
-    }
+
+      const respose = {
+        statusCode: HttpStatus.CREATED,
+        message: 'Task created successfully',
+        data: result,
+      };
+      return respose;
+    } catch (error) {}
   }
 
   async getTaskById(id: string) {
