2954ce46ce0a1401dac9a358f9e2b62accc2e044
claudiodantas <claudiodantas1996@gmail.com>
2025-10-31 22:01:49 -0300

    feat(auth-module): add async JWT module registration with secret and expiration configuration

--- apps/auth-service/src/auth/auth.module.ts
+++ apps/auth-service/src/auth/auth.module.ts
@@ -5,10 +5,23 @@ import { AuthController } from './auth.controller';
 import { LocalStrategy } from './local.strategy';
 import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserEntity } from '@repo/shared/entities/user';
+import { UserEntity } from 'src/db/entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    JwtModule.registerAsync({
+      global: true,
+      imports: [],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<number>('JWT_EXPIRATION_TIME'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
     UsersModule,
     PassportModule,
     TypeOrmModule.forFeature([UserEntity]),
