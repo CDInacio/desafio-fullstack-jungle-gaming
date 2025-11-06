e3da4f42bc1e1a261a03b3d2c0db9ab1eb341ab9
claudiodantas <claudiodantas1996@gmail.com>
2025-11-06 17:52:45 -0300

    refactor: simplify Dockerfiles by removing unused build stages and healthchecks feat: update web route to redirect to home and modify displayed text config: enable host and port settings in Vite configuration

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,15 +1,20 @@
 import CenteredContainer from "@/components/centered-container";
 import Layout from "@/components/layout";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
+  beforeLoad: () => {
+    throw redirect({
+      to: "/home",
+    });
+  },
   component: RouteComponent,
 });
 
 function RouteComponent() {
   return (
     <Layout>
-      <CenteredContainer className="text-input">s</CenteredContainer>
+      <CenteredContainer className="text-input">ola mundo</CenteredContainer>
     </Layout>
   );
 }
